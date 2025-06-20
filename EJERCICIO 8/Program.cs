af4688c07b4f9c0dd227814168630b9397c04373
Gleiser <75483922idexperujapon.edu.pe>
2025-06-20 17:34:43 -0500

    validar datos

--- EJERCICIO 8/Program.cs
+++ EJERCICIO 8/Program.cs
@@ -14,11 +14,18 @@ namespace EJERCICIO_8
             int nota;
             Console.WriteLine("ingrese una nota:");
             nota = Convert.ToInt32(Console.ReadLine());
+            while (nota<0 || nota>20)
+            {
+                Console.WriteLine("el valor es incorrecto");
+                Console.WriteLine("ingrese una nota entre 0 y 20:");
+                nota = Convert.ToInt32(Console.Read());
+            }
+
             if (nota >= 16)
             {
                 Console.WriteLine("exelente");
             }
-            else if (nota == 14 || nota == 15)
+            else if (nota == 14 && nota == 15)
             {
                 Console.WriteLine("muy bueno");
             }
