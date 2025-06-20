5f638129e88d4e97701c992edfb21ad26d6a0517
Gleiser <75483922idexperujapon.edu.pe>
2025-06-20 17:00:20 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO 8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            Console.WriteLine("ingrese una nota:");
+            nota = Convert.ToInt32(Console.ReadLine());
+            if (nota >= 16)
+            {
+                Console.WriteLine("exelente");
+            }
+            else if (nota == 14 || nota == 15)
+            {
+                Console.WriteLine("muy bueno");
+            }
+            else if (nota<= 13 && nota >= 11)
+            {
+                Console.WriteLine("bueno");
+            }
+            else
+            {
+                Console.WriteLine("insuficiente");
+            }
+                
+        }
+    }
+}
